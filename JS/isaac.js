4ef1c1602174e13ae2b70eee23c8c0ac8de5c87d
Efraín Sánchez <efrainchez@gmail.com>
2020-06-20 14:34:37 -0500

    Last commit to save changes before creating a new branch to try out some code

---
+++ JS/isaac.js
@@ -0,0 +1,13 @@
+// const imgs = document.getElementsByClassName("galery--imgs");
+
+// function template(img) {
+// 	return;
+// 	`
+//    <div class="overlay">
+//          <div class="modal" id="modal">
+//             <img src="${img}" alt="" />
+//          </div>
+//       </div>
+//    `;
+// }
+const logo = document.getElementById("logo");
