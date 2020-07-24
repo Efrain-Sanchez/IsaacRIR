3ba89a094709a3716b8e02a52aae79393e88468e
Efraín Sánchez <efrainchez@gmail.com>
2020-07-23 21:44:06 -0500

    ADD THE LOGIC OF JS CODE SO IMAGES IN GALERY PUPS UP AT A CLICK

---
+++ JS/array.js
@@ -0,0 +1,38 @@
+const objectArray [
+	img1 = {
+		ruta: "../IMG/experimento.jpeg",
+		tituloDeImagen: "EXPERIMENTO",
+		descripcion: "Preaparacion de Xanteno, colorante Eosina en Laboratorio",
+	},
+
+	img2 = {
+		ruta: "../IMG/interior.jpeg",
+		tituloDeImagen: "Interior de museo",
+		descripcion: "Lorem ipsum dolor ed itsen itum",
+	},
+
+	img3 = {
+		ruta: "../IMG/girasol.jpeg",
+		tituloDeImagen: "Girasoles en el campo",
+		descripcion: "Lorem ipsum dolor ed itsen itum"
+	},
+
+	img4 = {
+		ruta: ".../IMG/cloud.jpeg",
+		tituloDeImagen: "Cielo nublado",
+		descripcion: "Lorem ipsum dolor ed itsen itum",
+	},
+
+	img5 = {
+		ruta: "../IMG/pug.jpeg",
+		tituloDeImagen: "PUG",
+		descripcion: "Lorum Ipsum dolor et itum",
+	},
+
+	img6 = {
+		ruta: "../IMG/city.jpeg",
+		tituloDeImagen: "Panoramic city",
+		descripcion: "lorem ipsum dolor et itum",
+	},
+];
+export default objectArray;
--- JS/isaac.js
+++ JS/isaac.js
@@ -1,10 +1,47 @@
-console.log("Hola");
-const img1 = {
-	ruta: "../IMG/experimento.jpeg",
-	tituloDeImagen: "EXPERIMENTO",
-	descripcion: "Preaparacion de Xanteno, colorante Eosina en Laboratorio",
-};
-const callGenerator = generador(img1);
+const objectArray = [
+	(img1 = {
+		ruta: "../IMG/experimento.jpeg",
+		tituloDeImagen: "EXPERIMENTO",
+		descripcion: "Preaparacion de Xanteno, colorante Eosina en Laboratorio",
+		id: 1,
+	}),
+	(img2 = {
+		ruta: "../IMG/interior.jpeg",
+		tituloDeImagen: "Interior de museo",
+		descripcion: "Lorem ipsum dolor ed itsen itum",
+		id: 2,
+	}),
+	(img3 = {
+		ruta: "../IMG/girasol.jpeg",
+		tituloDeImagen: "Girasoles en el campo",
+		descripcion: "Lorem ipsum dolor ed itsen itum",
+		id: 3,
+	}),
+	(img4 = {
+		ruta: ".../IMG/cloud.jpeg",
+		tituloDeImagen: "Cielo nublado",
+		descripcion: "Lorem ipsum dolor ed itsen itum",
+		id: 4,
+	}),
+	(img5 = {
+		ruta: "../IMG/pug.jpeg",
+		tituloDeImagen: "PUG",
+		descripcion: "Lorum Ipsum dolor et itum",
+		id: 5,
+	}),
+	(img6 = {
+		ruta: "../IMG/city.jpeg",
+		tituloDeImagen: "Panoramic city",
+		descripcion: "lorem ipsum dolor et itum",
+		id: 1,
+	}),
+];
+
+const callGenerator = generador(objectArray[0]);
+const modal = $("#modal");
+const firstImg = $("#show");
+
+// firstImg.addEventListener("", setDisplay(modal));
 
 function generador(objeto) {
 	const img = $("#img").setAttribute("src", `${objeto.ruta}`);
@@ -16,4 +53,10 @@ function generador(objeto) {
 
 function $(selector) {
 	return document.querySelector(selector);
+}
+function setDisplay(objetoModal) {
+	objetoModal.style.display = "grid";
+}
+function setOutDisplay(objetoModal) {
+	objetoModal.style.display = "none";
 }
